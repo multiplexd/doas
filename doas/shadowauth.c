400437474d237c931c63b97b53e6083ec6a9e8b2
multiplex'd <multiplexd@gmx.com>
2017-04-25 06:41:00 +0100

    Get shadowauth right this time

--- doas/shadowauth.c
+++ doas/shadowauth.c
@@ -4,23 +4,20 @@
 #include <string.h>
 #include <bsd/string.h>
 
-/* The parsing of the salt done here is NOT portable! */
-
 int shadowauth(const char *u, const char *p) {
    struct spwd *spw = NULL;
-   char *salt = (char*) calloc(20, sizeof(char));
    char *res = NULL;
    
    if ((spw = getspnam(u)) != NULL) {
       strlcat(salt, spw->sp_pwdp, 21);
 
-      res = crypt(p, salt);
+      // crypt can use the salt from a previous encrypted password
+      res = crypt(p, spw->sp_pwdp); 
       if(strcmp(res, spw->sp_pwdp) != 0) {
 	 return 1;
       }
    }
 
-   free(salt);
    memset(spw, 0, sizeof(struct spwd));
    return 0;
 }
